75bd69d76a4b3ea0efa63952776082021d529154
Grama <Grama@DESKTOP-10DM7C7>
2025-11-17 22:25:27 +0200

    Add project files.

---
+++ Data/Grama_Razvan_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Grama_Razvan_Lab2.Models;
+
+namespace Grama_Razvan_Lab2.Data
+{
+    public class Grama_Razvan_Lab2Context : DbContext
+    {
+        public Grama_Razvan_Lab2Context (DbContextOptions<Grama_Razvan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grama_Razvan_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Grama_Razvan_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Grama_Razvan_Lab2.Data;
+using Grama_Razvan_Lab2.Models;
+
+namespace Grama_Razvan_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Grama_Razvan_Lab2.Data.Grama_Razvan_Lab2Context _context;
+
+        public IndexModel(Grama_Razvan_Lab2.Data.Grama_Razvan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+           
+        }
+    }
+}
